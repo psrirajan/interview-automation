2543f4d896d2a54f3cc21228af2fb49d38fd35c7
sowjanya kambam <sowjanya@autobooks.co>
2021-03-18 12:08:59 -0400

    updated staging URL

--- paymentForm.spec.js
+++ paymentForm.spec.js
@@ -3,7 +3,7 @@ const { browser } = require('protractor');
 describe('Payment Form', function () {
   describe('Credit Card Payment', function () {
     it('should submit a payment successfully', async function () {
-    await browser.get('https://staging.autobooks.co/pay/autobooks-inc-1');
+    await browser.get('https://staging.autobooks.co/pay/autobooks--ephrata-national-bank');
     // await expect(element(by.cssContainingText("*", "Make a contributon to Northeast Bank")).isPresent()).toBeTruthy("Payment header incorrect or not found")
       // TODO: implement your test here
     });
@@ -11,7 +11,7 @@ describe('Payment Form', function () {
   
   describe('ACH Payment', function () { 
     it('should submit a payment successfully', async function () {
-        await browser.get('https://staging.autobooks.co/pay/autobooks-inc-1');
+        await browser.get('https://staging.autobooks.co/pay/autobooks--ephrata-national-bank');
           // TODO: implement your test here
     });
   });
